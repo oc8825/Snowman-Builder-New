8f8531cb5f9f1bba47a4130c5f50a69b37a337f4
CarinaFucich <cf2663@princeton.edu>
2025-01-10 12:21:29 -0500

    more tilt

--- src/phaser/scenes/GameScene.jsx
+++ src/phaser/scenes/GameScene.jsx
@@ -185,6 +185,26 @@ class GameScene extends Phaser.Scene {
     }
 
     update() {
+        //tilt
+        if (typeof window.DeviceOrientationEvent !== 'undefined') {
+            window.addEventListener('deviceorientation', handleOrientation);
+        }
+        function handleOrientation(event) {
+            var x = event.beta;  // In degree in the range [-180,180]
+            var y = event.gamma; // In degree in the range [-90,90]
+            // Because we don't want to have the device upside down
+            // We constrain the x value to the range [-90,90]
+            if (x > 90) { x = 90 };
+            if (x < -90) { x = -90 };
+            // To make computation easier we shift the range of
+            // x and y to [0,180]
+            x += 90;
+            y += 90;
+            // 10 is half the size of the ball
+            // It center the position of the ball
+            ball.style.top = (window.innerHeight - 20) * x / 180 - 10 + 'px';
+            ball.style.left = (window.innerWidth - 20) * y / 180 - 10 + 'px';
+        }
         this.ground.tilePositionY -= 1;
 
         this.snowball.rotation += 0.01;
