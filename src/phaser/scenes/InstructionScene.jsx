0cd4105308e8f1d9326d47912dac12e2c7a5e034
CarinaFucich <cf2663@princeton.edu>
2025-01-16 16:06:39 -0500

    instrcutions pt2

--- src/phaser/scenes/InstructionScene.jsx
+++ src/phaser/scenes/InstructionScene.jsx
@@ -4,7 +4,7 @@ export default class InstructionScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('instructionBackground', '/src/assets/images/instructionBackground.jpg');
+        this.load.image('instructionBackground', '/src/assets/images/newinstructionbg.jpeg');
         this.load.image('arrow', '/src/assets/images/arrow.png');
     }
 
@@ -17,7 +17,7 @@ export default class InstructionScene extends Phaser.Scene {
 
         background.setScale(Math.max(this.scale.width / background.width, this.scale.height / background.height));
 
-        this.add.text(this.scale.width / 2, this.scale.height / 2 + 50, 'Tap the arrow to start!', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        this.add.text(this.scale.width / 2, this.scale.height / 2 + 50, 'Tap the arrow to go to the next instrcutions!', { fontSize: '32px', fill: '#000' }).setOrigin(0.5);
 
         this.arrow = this.add.image(this.scale.width / 2, this.scale.height / 2 + 200, 'arrow').setInteractive();
         this.arrow.setScale(0.5);
@@ -27,14 +27,44 @@ export default class InstructionScene extends Phaser.Scene {
             this.scene.start('ObjectiveScene'); 
         });
 
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 300, 'Instructions', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 250, 'To get started chose a player and enable tilt contorls on mobile devices.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 200, 'To move the snowball, tilt your device in the direction you want to move.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 150, 'Hitting snowballs gains points, hitting obsticles loses points.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 100, 'To build the body, get 10 points in part one, and then find your players shoes and pants.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2 - 50, 'To build the middle part of the snowman, get 7 points in part one, and then find your players shirt.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(this.scale.width / 2, this.scale.height / 2, 'To build the head of the snowman, get 5 points in part one, and then find your players head-gear.', { fontSize: '16px', fill: '#fff' }).setOrigin(0.5);
+        this.add.text(this.scale.width / 2, this.scale.height / 2 - 250, 'Instructions', { fontSize: '32px', fill: '#000' }).setOrigin(0.5);
 
+        const boxWidth = 500;
+
+        const verticalPosition = this.cameras.main.centerY - 100;
+      
+        const text = this.add.text(
+            this.cameras.main.centerX,
+            verticalPosition,
+            'To get started, choose a player and enable tilt controls on mobile devices. For desktop users, use the arrow keys to move the snowball. Hitting obstacles gains points, hitting obstacles loses points.',
+            { 
+                fontSize: '25px', 
+                color: '#000',
+                wordWrap: { width: boxWidth, useAdvancedWrap: true },
+                align: 'center',   
+             }
+       ).setOrigin(0.5);
+
+       const textBounds = text.getBounds();
+    
+
+       const box = this.add.graphics();
+       box.fillStyle(0x7EC8E3, 0.5);
+       
+
+       const borderRadius = 20;
+       box.fillRoundedRect(textBounds.x - 20, textBounds.y - 20, textBounds.width + 40, textBounds.height + 40, borderRadius);
+
+       box.lineStyle(2, 0x000000, 1);
+       box.strokeRoundedRect(
+              textBounds.x - 20,
+              textBounds.y - 20,
+              textBounds.width + 40,
+              textBounds.height + 40,
+              borderRadius
+       );
+
+       this.children.bringToTop(text);
        
     }
     hideInventory() {
